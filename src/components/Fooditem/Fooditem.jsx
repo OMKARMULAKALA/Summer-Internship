d36d22f60459fc544ee8c7225d012e34c746135a
MULAKALA OMKAR <omkarrsv11@gmail.com>
2024-06-10 14:37:04 +0530

    third commit

--- src/components/Fooditem/Fooditem.jsx
+++ src/components/Fooditem/Fooditem.jsx
@@ -1,10 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './Fooditem.css';
 import { assets } from '../../assets/assets';
 import { StoreContext } from '../../context/StoreContext';
 
 const Fooditem = ({ id, name, price, description, image }) => {
   const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+  const [rating, setRating] = useState(0);
+
+  const handleRating = (rate) => {
+    setRating(rate);
+  };
 
   return (
     <div className='food-item'>
@@ -14,7 +19,7 @@ const Fooditem = ({ id, name, price, description, image }) => {
           ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt='Add to cart' />
           : <div className='food-item-counter'>
               <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt='Remove from cart' />
-              <p>{cartItems[id]}</p> {/* Use square brackets for object access */}
+              <p>{cartItems[id]}</p>
               <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt='Add more' />
             </div>
         }
@@ -22,7 +27,17 @@ const Fooditem = ({ id, name, price, description, image }) => {
       <div className="food-item-info">
         <div className="food-item-name-rating">
           <p>{name}</p>
-          <img src={assets.rating_starts} alt='Rating' />
+          <div className="star-rating">
+            {[...Array(5)].map((_, index) => (
+              <span
+                key={index}
+                className={`star ${rating > index ? 'gold' : ''}`}
+                onClick={() => handleRating(index + 1)}
+              >
+                &#9733;
+              </span>
+            ))}
+          </div>
         </div>
         <p className="food-item-description">{description}</p>
         <p className="food-item-price">${price}</p>
